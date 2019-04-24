c38b420abfed20f9eec54f6c4ee7875326d68f1a
mooooon333 <742932097@qq.com>
2019-04-24 11:45:30 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ab97d506fb596023b3631669af503878.js"
+  "./precache-manifest.a2dc6234e09849b1bc61fdd62f057faa.js"
 );
 
 workbox.clientsClaim();

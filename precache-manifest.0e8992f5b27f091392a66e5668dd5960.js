9f14c298cc66c7e590cedab4a4fd6467702fc81d
mooooon333 <742932097@qq.com>
2019-04-24 11:14:21 +0800

    Updates

---
+++ precache-manifest.0e8992f5b27f091392a66e5668dd5960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5dfdea02ce6d326c9c5",
+    "url": "/movielist/static/js/runtime~main.adab6e74.js"
+  },
+  {
+    "revision": "d6986fed9ed421fb329c",
+    "url": "/movielist/static/js/main.5e1b74c5.chunk.js"
+  },
+  {
+    "revision": "77ad2e7043e8a68d55dc",
+    "url": "/movielist/static/js/2.041efe3f.chunk.js"
+  },
+  {
+    "revision": "d6986fed9ed421fb329c",
+    "url": "/movielist/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "77ad2e7043e8a68d55dc",
+    "url": "/movielist/static/css/2.3c1889a4.chunk.css"
+  },
+  {
+    "revision": "63fb5569ce605d654cefa01328820154",
+    "url": "/movielist/index.html"
+  }
+];
